fed991e0d813a7fe160035999f9cfdc82b45ce8c
Irian <durianirian@gmail.com>
2022-06-14 13:20:05 -0600

    Create Calculate Current Profit or Loss.cpp

---
+++ Calculate Current Profit or Loss.cpp
@@ -0,0 +1,15 @@
+/* Given: A 40 bottle case of wine was purchased for $2000.
+       A cold storage fee is $15 per day for the case. 
+       These bottles will are to be sold at a single future event and will be in cold storage until the event.  
+       Any unsold bottles are considered spoilage. */
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numSold; //number of bottles sold
+    float sellingPrice; //selling price of the bottles
+    int days; //number of days in storage
+    int profit; //the profit or loss.
+    
+}
