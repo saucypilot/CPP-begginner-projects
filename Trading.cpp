4437e8215c130270cd684a16018392094b939a11
saucypilot <durianirian@gmail.com>
2022-06-24 14:00:22 -0500

    Create Trading.cpp

---
+++ Trading.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char menuChoice
+
+    cout<<" ********************************************************************************"<< endl;
+    cout<<"     A. Buy Bonds                B. Sell Bonds"<<endl<<endl;
+    cout<<"     C. Buy Stocks               D. Sell Stocks       E. Short Stock Sale"<<endl<<endl;
+    cout<<"     X. Exit";
+
+    cout<<"From the menu above, choose how you want to interact with the market by entering the letter of your corressponding choice."<<endl;
+    cout<<"(Make sure it's capitalize)"<<endl<<endl;
+    cout<<"Enter here: "
+    cin>>menuChoice;
+
+    return 0;
+}
