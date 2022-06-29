18b08c12927ccccfaf8c3e06b1bed4dace22547e
saucypilot <durianirian@gmail.com>
2022-06-29 17:59:37 -0500

    Create Fixed Compound Interest Investment.cpp

---
+++ Fixed Compound Interest Investment.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double balance;
+    int years;
+    double apr;
+
+    cout<<"Enter Balance: ";
+    cin>>balance;
+    cout<<"Enter Interest Rate: ";
+    cin>>interest;
+    cout<<"Enter years: ";
+    cin>>years;
+    cout<<endl;
+
+    cout<<"Year   Balance";
+    
+}
