a4ac346486937966e90f4d2285443e0aa9f1548d
saucypilot <durianirian@gmail.com>
2022-06-29 19:34:43 -0500

    Update Fixed Compound Interest Investment.cpp

--- Fixed Compound Interest Investment.cpp
+++ Fixed Compound Interest Investment.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
 int main(){
     double balance;
     int years;
+    int perYear;
     double apr;
 
     cout<<"Enter Balance: ";
     cin>>balance;
     cout<<"Enter Interest Rate: ";
-    cin>>interest;
+    cin>>apr;
     cout<<"Enter years: ";
     cin>>years;
     cout<<endl;
 
     cout<<"Year   Balance";
-    
+    for(perYear=0; perYear<=years; perYear++){
+        cout << fixed << setprecision(2)<< perYear << "        "<< balance << endl;
+        balance = balance * ( 1 + apr) ; 
+    }
+    cout<<endl<<"Done!";
+    return 0;
 }
