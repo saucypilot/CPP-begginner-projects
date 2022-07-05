71bbd8e258270e967f19caf57a6f3597aad7614a
saucypilot <durianirian@gmail.com>
2022-07-05 18:14:37 -0500

    Had to use a project because this program needed to use a another one

---
+++ Reading a file project/Reading a file.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream inputFile;
+    const string num1, num2 ,num3, num4, num5;
+
+    inputFile.open("Week10a.dat")
+
+    
+}
