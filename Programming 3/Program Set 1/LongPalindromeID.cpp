99d13e005dd877668c6f34eb5c84bd20478d7508
Irian Durian <durianirian@gmail.com>
2023-02-23 21:38:15 -0600

    Update LongPalindromeID.cpp

--- Programming 3/Program Set 1/LongPalindromeID.cpp
+++ Programming 3/Program Set 1/LongPalindromeID.cpp
@@ -1,66 +1,57 @@
-    #include <iostream>
-    #include <string>
-    #include <algorithm>
+#include <iostream>
+#include <string>
+#include <algorithm>
 
-    using namespace std;
+using namespace std;
 
-    // contains the longest palindrome
-    struct Palindrome
+struct Palindrome
+{
+    string longest;
+};
+
+bool isPalindrome(const string &str)
+{
+    return str == string(str.rbegin(), str.rend());
+}
+
+Palindrome findLongestPalindrome(const string &str)
+{
+    Palindrome palindrome;
+    palindrome.longest = "";
+
+    int n = str.length();
+
+    for (int i = 0; i < n; i++)
     {
-        string longest;
-    };
-
-    // checks if a given string is a palindrome or not
-    bool isPalindrome(const string &str)
-    {
-        return str == string(str.rbegin(), str.rend());
-    }
-
-    Palindrome findLongestPalindrome(const string &str)
-    {
-        Palindrome palindrome;
-        palindrome.longest = "";
-
-        int n = str.length();
-
-        for (int i = 0; i < n; i++)
+        for (int j = i; j < n; j++)
         {
-            for (int j = i; j < n; j++)
+            string substring = str.substr(i, j - i + 1);
+            if (isPalindrome(substring) && substring.length() > palindrome.longest.length())
             {
-                string substring = str.substr(i, j - i + 1);
-                if (isPalindrome(substring) && substring.length() > palindrome.longest.length())
-                {
-                    palindrome.longest = substring;
-                }
+                palindrome.longest = substring;
             }
         }
-
-        return palindrome;
     }
 
-    // prompts the user for input and calls the `findLongestPalindrome` function
-    void runProgram()
+    return palindrome;
+}
+
+int main()
+{
+    char choice;
+    do
     {
         string input;
-        char choice;
+        cout << "Enter a string: ";
+        cin >> input;
 
-        do
-        {
-            cout << "Enter a string: ";
-            cin >> input;
+        Palindrome result = findLongestPalindrome(input);
 
-            Palindrome result = findLongestPalindrome(input);
+        cout << "Longest palindrome: " << result.longest << endl;
 
-            cout << "The longest palindrome in the given string is: " << result.longest << endl;
+        cout << "Run again (Y/N): ";
+        cin >> choice;
+    } while (tolower(choice) == 'y');
 
-            cout << "Do you want to run the program again? (y/n): ";
-            cin >> choice;
-
-        } while (tolower(choice) == 'y');
-    }
-
-    int main()
-    {
-        runProgram();
-        return 0;
-    }
+    return 0;
+}
