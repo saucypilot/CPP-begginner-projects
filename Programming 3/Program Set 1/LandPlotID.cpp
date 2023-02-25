d1a0d81c3aabd13fcf9027c69c4ccf07b4971494
Irian Durian <durianirian@gmail.com>
2023-02-24 19:46:39 -0600

    Update LandPlotID.cpp

--- Programming 3/Program Set 1/LandPlotID.cpp
+++ Programming 3/Program Set 1/LandPlotID.cpp
@@ -1,167 +1,63 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <vector>
 #include <algorithm>
+
 using namespace std;
 
-const int NUM_ROWS = 8;
-const int NUM_COLS = 16;
-const int NUM_PLOTS = 128;
-const int ACRES_PER_PLOT = 5;
-const int MAX_ROW = 8;
-const int MAX_COL = 16;
-const int MAX_ACRES = 128;
-const char UNCLAIMED = '*';
-
-// Data structure to represent a lot of land
-struct Lot {
-    int area; // area in acres
-    bool visited; // flag to mark if lot has been visited during processing
-};
-
-// Function to compute the area of a lot of land
-int computeLotArea(vector<vector<char>>& grid, int row_start, int row_end, int col_start, int col_end) {
-    int num_unclaimed_plots = 0;
-    for (int i = row_start; i <= row_end; i++) {
-        for (int j = col_start; j <= col_end; j++) {
-            if (grid[i][j] == '*') {
-                num_unclaimed_plots++;
-            }
-        }
-    }
-    return num_unclaimed_plots * ACRES_PER_PLOT;
-}
-
-// Function to determine if a lot of land is completely surrounded by claimed plots
-bool isEnclosedLot(vector<vector<char>>& grid, int row_start, int row_end, int col_start, int col_end) {
-    // Check if any border plot is unclaimed
-    for (int i = row_start; i <= row_end; i++) {
-        if (grid[i][col_start] == '*' || grid[i][col_end] == '*') {
-            return false;
-        }
-    }
-    for (int j = col_start; j <= col_end; j++) {
-        if (grid[row_start][j] == '*' || grid[row_end][j] == '*') {
-            return false;
-        }
-    }
-    return true;
-}
-
-// Function to traverse and mark all plots belonging to a specific lot of land
-void traverseLot(vector<vector<char>>& grid, vector<vector<Lot>>& lots, int row, int col, int lot_num) {
-    if (row < 0 || row >= NUM_ROWS || col < 0 || col >= NUM_COLS || lots[row][col].visited) {
-        return;
-    }
-    if (grid[row][col] != '*') {
-        return;
-    }
-    lots[row][col].visited = true;
-    lots[row][col].area = ACRES_PER_PLOT;
-    if (row > 0) {
-        traverseLot(grid, lots, row - 1, col, lot_num);
-    }
-    if (row < NUM_ROWS - 1) {
-        traverseLot(grid, lots, row + 1, col, lot_num);
-    }
-    if (col > 0) {
-        traverseLot(grid, lots, row, col - 1, lot_num);
-    }
-    if (col < NUM_COLS - 1) {
-        traverseLot(grid, lots, row, col + 1, lot_num);
-    }
-}
-
-void determineLotSize(char grid[][MAX_COL], int row, int col, int &lotSize, int startRow, int startCol);
-
-// Function to process a single survey
-void processSurvey(int n, ifstream &infile)
+int main()
 {
-    for (int i = 1; i <= n; i++)
+    string fileName;
+    cout << "Enter the file name: ";
+    cin >> fileName;
+    ifstream inFile;
+    inFile.open(fileName);
+    if (!inFile)
     {
-        char grid[MAX_ROW][MAX_COL];
-        int numUnclaimed = 0;
-        int acres[MAX_ACRES] = {0};
-
-        // Read the section grid from the file
-        for (int row = 0; row < MAX_ROW; row++)
+        cout << "Unable to open file";
+        exit(1); // terminate with error
+    }
+    int n;
+    inFile >> n;
+    inFile.ignore();
+    for (int i = 0; i < n; i++)
+    {
+        vector<int> v;
+        string line;
+        for (int j = 0; j < 8; j++)
         {
-            for (int col = 0; col < MAX_COL; col++)
+            getline(inFile, line);
+            int count = 0;
+            for (int k = 0; k < 16; k++)
             {
-                infile >> grid[row][col];
-                if (grid[row][col] == UNCLAIMED)
+                if (line[k] == '*')
                 {
-                    numUnclaimed++;
+                    count++;
+                }
+                else
+                {
+                    if (count > 0)
+                    {
+                        v.push_back(count);
+                        count = 0;
+                    }
                 }
             }
-        }
-
-        // Determine the size of each unclaimed lot
-        int lotNum = 0;
-        for (int row = 0; row < MAX_ROW; row++)
-        {
-            for (int col = 0; col < MAX_COL; col++)
+            if (count > 0)
             {
-                if (grid[row][col] == UNCLAIMED)
-                {
-                    int lotSize = 0;
-                    determineLotSize(grid, row, col, lotSize, row, col);
-                    acres[lotSize]++;
-                }
+                v.push_back(count);
             }
         }
-
-        // Output the results for the current survey
-        cout << "Survey " << i << ":";
-        for (int j = MAX_ACRES - 1; j >= 0; j--)
+        sort(v.begin(), v.end(), greater<int>());
+        cout << "Survey " << i + 1 << ": ";
+        for (int j = 0; j < v.size(); j++)
         {
-            for (int k = 0; k < acres[j]; k++)
-            {
-                cout << " " << j * 5;
-            }
+            cout << v[j] * 5 << " ";
         }
         cout << endl;
-
-        // Skip the blank line after each section grid except for the last one
-        if (i != n)
-        {
-            string blankLine;
-            getline(infile, blankLine);
-        }
+        getline(inFile, line);
     }
-}
-
-void determineLotSize(char grid[][MAX_COL], int row, int col, int &acreCount, int startRow, int startCol)
-{
-    if (grid[row][col] != '*')
-    {
-        return; // the current plot has already been claimed
-    }
-
-    // mark the current plot as visited
-    grid[row][col] = '#';
-
-    // add 5 acres to the current lot
-    acreCount += 5;
-
-    // check the neighboring plots
-    if (row > startRow && grid[row - 1][col] == '*')
-    {
-        determineLotSize(grid, row - 1, col, acreCount, startRow, startCol);
-    }
-
-    if (row < MAX_ROW - 1 && grid[row + 1][col] == '*')
-    {
-        determineLotSize(grid, row + 1, col, acreCount, startRow, startCol);
-    }
-
-    if (col > startCol && grid[row][col - 1] == '*')
-    {
-        determineLotSize(grid, row, col - 1, acreCount, startRow, startCol);
-    }
-
-    if (col < MAX_COL - 1 && grid[row][col + 1] == '*')
-    {
-        determineLotSize(grid, row, col + 1, acreCount, startRow, startCol);
-    }
+    inFile.close();
+    return 0;
 }
