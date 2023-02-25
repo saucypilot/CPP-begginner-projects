18b1fe4bb0e1ed1d00099ca00b6efd25cfebcd71
Irian Durian <durianirian@gmail.com>
2023-02-25 10:47:18 -0600

    Wrong name for file

--- Programming 3/Program Set 1/BalChemEquationsID.cpp
+++ Programming 3/Program Set 1/BalChemEquationsID.cpp
@@ -1,18 +1,3 @@
-/*
-Write a program to balance chemical equations. An equation is comprised of a list of compounds and elements, 
-separated by plus-signs, on both sides of an equal sign. An element will be represented by either one or two letters; 
-the first is always capitalized, and if the element has a second character, it will be lowercase. Compounds are composed of 
-several elements added together that may have parenthesis around them and with a number following each element. 
-This number represents the number of atoms of the element in front of the number in the compound. If there is only one 
-atom of an element in the compound, no number will appear after the element. The result is m the lowest number of integer 
-coefficients for each compound needed so the equation is balanced. An equation is balanced when the number of atoms of each 
-element on the left side is equal to that of the right side. Coefficients are applied to the complete compound; so, a 
-coefficient of two for FeCl represents two atoms of Fe and two atoms of Cl. Input is from a text file, where the first 
-line contains an integer N between $[1,30]$. The following N lines define equations according to the rules described above. 
-For each labeled equation, output to the screen the balanced equation with the lowest integer number coefficients. If an 
-equation cannot be balanced correctly or uniquely, output the words "Cannot Balance".
-Let the user enter the file namefrom the keyboard. Refer to the sample output below.
- */
 #include <iostream>
 #include <fstream>
 #include <string>
