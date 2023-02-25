cd1bdcdd8168dda8d64833124518de9fd5270e91
Irian Durian <durianirian@gmail.com>
2023-02-25 13:03:29 -0600

    Update BalChemEquationsID.cpp

--- Programming 3/Program Set 1/BalChemEquationsID.cpp
+++ Programming 3/Program Set 1/BalChemEquationsID.cpp
@@ -1,344 +1,277 @@
-/*
-Write a program to balance chemical equations. An equation is comprised of a list of compounds and elements, separated by plus-signs, on both sides of an equal sign. An element will be represented by either one or two letters; the first is always capitalized, and if the element has a second character, it will be lowercase. Compounds are composed of several elements added together that may have parenthesis around them and with a number following each element. This number represents the number of atoms of the element in front of the number in the compound. If there is only one atom of an element in the compound, no number will appear after the element. The result is m the lowest number of integer coefficients for each compound needed so the equation is balanced. An equation is balanced when the number of atoms of each element on the left side is equal to that of the right side. Coefficients are applied to the complete compound; so, a coefficient of two for FeCl represents two atoms of Fe and two atoms of Cl. Input is from a text file, where the first line contains an integer N between $[1,30]$. The following N lines define equations according to the rules described above. For each labeled equation, output to the screen the balanced equation with the lowest integer number coefficients. If an equation cannot be balanced correctly or uniquely, output the words "Cannot Balance". Let the user enter the file namefrom the keyboard. Refer to the sample output below.
-
-Sample File:
-```
-5
-Fe + Cl2 = FeCl3
-KMnO4 + HCl = KCl + MnCl2 + H2O + Cl2
-C8H18 + O2 = CO2 + H2O
-Fe2(SO4)3 KSCN = K3Fe(SCN)6 K2SO4
-CO2 = CO
-```
-
-Sample Run:
-```
-Enter the file name: balequation.txt
-
-E1: 2Fe + 3Cl2 = 2FeCl3
-E2: 2KMnO4 + 16HCl = 2KCl + 2MnCl2 + 8H2O + 5Cl2
-E3: 2C8H18 + 25O2 = 16CO2 + 18H2O
-E4: Fe2(SO4)3 + 12KSCN = 2K3Fe(SCN)6 + 3K2SO4
-E5: Cannot Balance
-```
- */
-
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <vector>
-#include <map>
+#include <unordered_map>
+#include <algorithm>
+#include <numeric>
 
 using namespace std;
 
-// Function to parse a chemical equation string into its constituent parts
-void parseEquation(const std::string &equation, std::vector<std::map<std::string, int>> &compounds);
-
-// Function to extract the elements and their counts from a compound string
-void parseCompound(const std::string &compound, std::map<std::string, int> &elements);
-
-// Function to balance a chemical equation
-bool balanceEquation(std::vector<std::map<std::string, int>> &reactants,
-                     std::vector<std::map<std::string, int>> &products,
-                     std::vector<int> &coefficients);
-
-// Function to print the balanced equation
-void printBalancedEquation(const std::string &equation,
-                           const std::vector<std::map<std::string, int>> &reactants,
-                           const std::vector<std::map<std::string, int>> &products,
-                           const std::vector<int> &coefficients);
-
-// Function to print "Cannot Balance" message for unbalanced equations
-void printCannotBalance(const std::string &equation);
-
-int main()
+// Function to parse a compound string into its elements and coefficients
+unordered_map<string, int> parseCompound(string compound)
 {
-    // Open the input file
-    std::string filename;
-    std::cout << "Enter the file name: ";
-    std::cin >> filename;
-    std::ifstream infile(filename);
-    if (!infile)
+    unordered_map<string, int> elements;
+    string element = "";
+    int coeff = 1;
+
+    for (int i = 0; i < compound.length(); i++)
     {
-        std::cerr << "Error: Cannot open file " << filename << std::endl;
-        return 1;
-    }
-
-    // Read the number of equations
-    int numEquations;
-    infile >> numEquations;
-
-    // Read and balance each equation
-    std::string equation;
-    std::getline(infile, equation); // Skip the newline character
-    for (int i = 0; i < numEquations; ++i)
-    {
-        // Read the equation
-        std::getline(infile, equation);
-
-        // Parse the equation into reactants and products
-        std::vector<std::map<std::string, int>> reactants;
-        std::vector<std::map<std::string, int>> products;
-        parseEquation(equation, reactants);
-        parseEquation(equation, products);
-
-        // Balance the equation
-        std::vector<int> coefficients;
-        if (balanceEquation(reactants, products, coefficients))
+        if (isupper(compound[i]))
         {
-            // Print the balanced equation
-            printBalancedEquation(equation, reactants, products, coefficients);
+            // Start of a new element
+            if (element != "")
+            {
+                // Update the map with the previous element and its coefficient
+                elements[element] += coeff;
+                element = "";
+                coeff = 1;
+            }
+            element += compound[i];
         }
-        else
+        else if (islower(compound[i]))
         {
-            // Print "Cannot Balance" message
-            printCannotBalance(equation);
+            // Continuation of the previous element
+            element += compound[i];
         }
-    }
-
-    return 0;
-}
-
-// Function to parse a chemical equation string into its constituent parts
-void parseEquation(const std::string &equation, std::vector<std::map<std::string, int>> &compounds)
-{
-    // Find the equal sign
-    std::string::size_type equalSignPos = equation.find('=');
-
-    // Parse the reactants
-    std::string::size_type pos = 0;
-    while (pos < equalSignPos)
-    {
-        // Find the next plus sign
-        std::string::size_type plusSignPos = equation.find('+', pos);
-
-        // Parse the compound
-        std::map<std::string, int> elements;
-        parseCompound(equation.substr(pos, plusSignPos - pos), elements);
-        compounds.push_back(elements);
-
-        // Move to the next compound
-        pos = plusSignPos + 1;
-    }
-
-    // Parse the products
-    pos = equalSignPos + 1;
-    while (pos < equation.length())
-    {
-        // Find the next plus sign
-        std::string::size_type plusSignPos = equation.find('+', pos);
-
-        // Parse the compound
-        std::map<std::string, int> elements;
-        parseCompound(equation.substr(pos, plusSignPos - pos), elements);
-        compounds.push_back(elements);
-
-        // Move to the next compound
-        pos = plusSignPos + 1;
-    }
-}
-
-// Function to extract the elements and their counts from a compound string
-void parseCompound(const std::string &compound, std::map<std::string, int> &elements)
-{
-    // Find the first parenthesis
-    std::string::size_type leftParenPos = compound.find('(');
-
-    // If there is no parenthesis, parse the compound as a single element
-    if (leftParenPos == std::string::npos)
-    {
-        // Find the first digit
-        std::string::size_type digitPos = compound.find_first_of("0123456789");
-
-        // If there is no digit, the compound is a single element
-        if (digitPos == std::string::npos)
+        else if (isdigit(compound[i]))
         {
-            elements[compound] = 1;
+            // Coefficient of the current element
+            coeff = compound[i] - '0';
+            while (i + 1 < compound.length() && isdigit(compound[i + 1]))
+            {
+                // Handle multi-digit coefficients
+                coeff = coeff * 10 + (compound[i + 1] - '0');
+                i++;
+            }
         }
-        else
+        else if (compound[i] == '(')
         {
-            // Extract the element and its count
-            std::string element = compound.substr(0, digitPos);
-            int count = std::stoi(compound.substr(digitPos));
-            elements[element] = count;
+            // Start of a compound within parenthesis
+            string subCompound = "";
+            int j = i + 1;
+            int count = 1;
+            while (count != 0)
+            {
+                if (compound[j] == '(')
+                {
+                    count++;
+                }
+                else if (compound[j] == ')')
+                {
+                    count--;
+                }
+                subCompound += compound[j];
+                j++;
+            }
+            i = j - 1;
+
+            unordered_map<string, int> subElements = parseCompound(subCompound);
+            for (auto it = subElements.begin(); it != subElements.end(); it++)
+            {
+                elements[it->first] += it->second * coeff;
+            }
         }
     }
-    else
-    {
-        // Find the matching right parenthesis
-        std::string::size_type rightParenPos = compound.find(')', leftParenPos);
 
-        // Parse the compound inside the parenthesis
-        std::map<std::string, int> subElements;
-        parseCompound(compound.substr(leftParenPos + 1, rightParenPos - leftParenPos - 1), subElements);
+    // Handle the last element
+    elements[element] += coeff;
 
-        // Find the first digit after the right parenthesis
-        std::string::size_type digitPos = compound.find_first_of("0123456789", rightParenPos);
-
-        // If there is no digit, the count is 1
-        int count = 1;
-        if (digitPos != std::string::npos)
-        {
-            // Extract the count
-            count = std::stoi(compound.substr(digitPos));
-        }
-
-        // Add the elements to the compound
-        for (auto &subElement : subElements)
-        {
-            elements[subElement.first] = subElement.second * count;
-        }
-    }
+    return elements;
 }
 
 // Function to balance a chemical equation
-bool balanceEquation(std::vector<std::map<std::string, int>> &reactants,
-                     std::vector<std::map<std::string, int>> &products,
-                     std::vector<int> &coefficients)
+string balanceEquation(string equation)
 {
-    // Create a map of elements and their counts
-    std::map<std::string, int> elements;
+    // Split the equation into left and right sides
+    vector<string> sides = splitEquation(equation);
 
-    // Add the reactants to the map
-    for (auto &reactant : reactants)
+    // Parse the compounds on both sides
+    map<string, int> leftCompounds = parseCompounds(sides[0]);
+    map<string, int> rightCompounds = parseCompounds(sides[1]);
+
+    // Get the list of all elements in the equation
+    set<string> elements;
+    for (auto const &compound : leftCompounds)
     {
-        for (auto &element : reactant)
+        for (auto const &element : parseElements(compound.first))
         {
-            elements[element.first] += element.second;
+            elements.insert(element);
+        }
+    }
+    for (auto const &compound : rightCompounds)
+    {
+        for (auto const &element : parseElements(compound.first))
+        {
+            elements.insert(element);
         }
     }
 
-    // Subtract the products from the map
-    for (auto &product : products)
+    // Create the matrix of coefficients for each element in each compound
+    int nElements = elements.size();
+    int nCompounds = leftCompounds.size() + rightCompounds.size();
+    Matrix matrix(nElements, nCompounds);
+    int col = 0;
+    for (auto const &compound : leftCompounds)
     {
-        for (auto &element : product)
+        for (auto const &element : parseElements(compound.first))
         {
-            elements[element.first] -= element.second;
+            matrix.setElement(getElementIndex(elements, element), col, parseCount(compound.first, element));
         }
+        col++;
+    }
+    for (auto const &compound : rightCompounds)
+    {
+        for (auto const &element : parseElements(compound.first))
+        {
+            matrix.setElement(getElementIndex(elements, element), col, -parseCount(compound.first, element));
+        }
+        col++;
     }
 
-    // Check if the equation is balanced
-    for (auto &element : elements)
+    // Solve the system of linear equations
+    vector<int> coefficients = matrix.solve();
+
+    // Check if the system has no solution or more than one solution
+    if (coefficients.empty())
     {
-        if (element.second != 0)
-        {
-            return false;
-        }
+        return "Cannot Balance";
+    }
+    else if (coefficients.size() != nCompounds)
+    {
+        return "Multiple Solutions";
     }
 
-    // Create a map of elements and their coefficients
-    std::map<std::string, int> elementCoefficients;
-
-    // Add the reactants to the map
-    for (auto &reactant : reactants)
+    // Construct the balanced equation using the coefficients
+    string balancedEquation;
+    col = 0;
+    for (auto const &compound : leftCompounds)
     {
-        for (auto &element : reactant)
+        if (col > 0)
         {
-            if (elementCoefficients[element.first] < element.second)
-            {
-                elementCoefficients[element.first] = element.second;
-            }
+            balancedEquation += " + ";
         }
+        balancedEquation += toCoeffString(coefficients[col]) + compound.first;
+        col++;
+    }
+    balancedEquation += " = ";
+    for (auto const &compound : rightCompounds)
+    {
+        if (col > 0)
+        {
+            balancedEquation += " + ";
+        }
+        balancedEquation += toCoeffString(coefficients[col]) + compound.first;
+        col++;
     }
 
-    // Add the products to the map
-    for (auto &product : products)
+    // Combine the two maps into a single map of all compounds
+    Map<String, Integer> compounds = new HashMap<>(leftCompounds);
+    rightCompounds.forEach((k, v)->compounds.merge(k, v, Integer::sum));
+
+    // Build the matrix of coefficients
+    int[][] matrix = new int[compounds.size()][compounds.size() + 1];
+
+    // Fill in the coefficients for each compound on the left side
+    int col = 0;
+    for (String compound : leftCompounds.keySet())
     {
-        for (auto &element : product)
-        {
-            if (elementCoefficients[element.first] < element.second)
-            {
-                elementCoefficients[element.first] = element.second;
-            }
-        }
+        fillInCoefficients(matrix, col, parseCompound(compound), compounds);
+        col++;
     }
 
-    // Calculate the coefficients
-    for (auto &reactant : reactants)
+    // Fill in the coefficients for each compound on the right side
+    for (String compound : rightCompounds.keySet())
     {
-        int coefficient = 1;
-        for (auto &element : reactant)
-        {
-            int elementCoefficient = elementCoefficients[element.first] / element.second;
-            if (coefficient < elementCoefficient)
-            {
-                coefficient = elementCoefficient;
-            }
-        }
-        coefficients.push_back(coefficient);
+        fillInCoefficients(matrix, col, parseCompound(compound), compounds);
+        col++;
     }
 
-    for (auto &product : products)
+    // Solve the matrix to get the coefficients
+    int[] coefficients = solveMatrix(matrix);
+
+    // Check if the equation could be balanced
+    if (coefficients == null)
     {
-        int coefficient = 1;
-        for (auto &element : product)
-        {
-            int elementCoefficient = elementCoefficients[element.first] / element.second;
-            if (coefficient < elementCoefficient)
-            {
-                coefficient = elementCoefficient;
-            }
-        }
-        coefficients.push_back(coefficient);
+        return "Cannot Balance";
     }
 
-    return true;
+    // Build the balanced equation
+    StringBuilder balancedEquation = new StringBuilder();
+    col = 0;
+    for (String compound : leftCompounds.keySet())
+    {
+        int coefficient = coefficients[col];
+        if (coefficient > 1)
+        {
+            balancedEquation.append(coefficient);
+        }
+        balancedEquation.append(compound);
+        balancedEquation.append(" + ");
+        col++;
+    }
+    for (String compound : rightCompounds.keySet())
+    {
+        int coefficient = coefficients[col];
+        if (coefficient > 1)
+        {
+            balancedEquation.append(coefficient);
+        }
+        balancedEquation.append(compound);
+        balancedEquation.append(" + ");
+        col++;
+    }
+    balancedEquation.delete(balancedEquation.length() - 3, balancedEquation.length());
+
+    return balancedEquation.toString();
 }
 
-// Function to print a balanced chemical equation
-void printBalancedEquation(const std::string &equation,
-                           const std::vector<std::map<std::string, int>> &reactants,
-                           const std::vector<std::map<std::string, int>> &products,
-                           const std::vector<int> &coefficients)
+/**
+ * Parses a compound into a map of elements and their counts.
+ *
+ * @param compound the compound to parse
+ * @return a map of elements and their counts
+ */
+private
+static Map<String, Integer> parseCompound(String compound)
 {
-    // Print the reactants
-    for (std::size_t i = 0; i < reactants.size(); ++i)
+    Map<String, Integer> elements = new HashMap<>();
+    Matcher matcher = ELEMENT_PATTERN.matcher(compound);
+    while (matcher.find())
     {
-        // Print the coefficient
-        std::cout << coefficients[i];
-
-        // Print the compound
-        for (auto &element : reactants[i])
-        {
-            std::cout << element.first;
-            if (element.second > 1)
-            {
-                std::cout << element.second;
-            }
-        }
-
-        // Print the plus sign
-        if (i < reactants.size() - 1)
-        {
-            std::cout << " + ";
-        }
+        String element = matcher.group(1);
+        String count = matcher.group(2);
+        int coefficient = count != null ? Integer.parseInt(count) : 1;
+        elements.merge(element, coefficient, Integer::sum);
     }
-
-    // Print the equal sign
-    std::cout << " = ";
-
-    // Print the products
-    for (std::size_t i = 0; i < products.size(); ++i)
-    {
-        // Print the coefficient
-        std::cout << coefficients[i + reactants.size()];
-
-        // Print the compound
-        for (auto &element : products[i])
-        {
-            std::cout << element.first;
-            if (element.second > 1)
-            {
-                std::cout << element.second;
-            }
-        }
-
-        // Print the plus sign
-        if (i < products.size() - 1)
-        {
-            std::cout << " + ";
-        }
-    }
-
-    std::cout << std::endl;
+    return elements;
 }
+
+/**
+ * Fills in the coefficients for a compound in the matrix.
+ *
+ * @param matrix    the matrix to fill in
+ * @param col       the column to fill in
+ * @param compound  the compound to fill in
+ * @param compounds the map of all compounds and their elements
+ */
+private
+static void fillInCoefficients(int[][] matrix, int col, Map<String, Integer> compound,
+                               Map<String, Integer> compounds)
+{
+    for (Map.Entry<String, Integer> entry : compound.entrySet())
+    {
+        String element = entry.getKey();
+        int row = getRowForElement(element, compounds);
+        int count = entry.getValue();
+        matrix[row][col] = count;
+    }
+    matrix[matrix.length - 1][col] = -1;
+}
+/**
+     * Solves the matrix to get the coefficients for each compound.
+     *
+     * @param matrix the matrix to solve
+     * @return an array of coefficients
+     * @throws IllegalArgumentException if the matrix cannot be solved
+     */
