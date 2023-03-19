fbe9117ddb830aee76fe3f6afb33092671ae8917
Irian Durian <durianirian@gmail.com>
2023-03-19 00:46:38 -0500

    Update LListMenuID.cpp

--- Programming 3/Program set 2/LListMenuID.cpp
+++ Programming 3/Program set 2/LListMenuID.cpp
@@ -15,7 +15,7 @@ struct Node
 
 //function prototypes for the linked list operations
 void insert(Node *&head, int key);
-void delete(Node *&head, int key);
+void remove(Node *&head, int key);
 void search(Node *head, int key);
 void print(Node *head);
 void sort(Node *&head);
@@ -25,6 +25,7 @@ void iShift(Node *&head, int k);
 void clear(Node *&head);
 
 //function prototypes for the menu
+
 
 int main()
 {
@@ -43,10 +44,169 @@ Linked list operations
 */
 void insert(Node *&head, int key)
 {
-    //create a new node
     Node *newNode = new Node(key);
 
-    //insert the new node at the beginning of the list
     newNode->next = head;
     head = newNode;
 }
+
+void remove(Node *&head, int key)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+        if (curr->key == key)
+        {
+            if (prev == NULL)
+            {
+                head = curr->next;
+            }
+            else
+            {
+                prev->next = curr->next;
+            }
+            delete curr;
+            return;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+}
+
+void search(Node *head, int key)
+{
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+        if (curr->key == key)
+        {
+            cout << "Found " << key << endl;
+            return;
+        }
+        curr = curr->next;
+    }
+    cout << "Not found " << key << endl;
+}
+
+void print(Node *head)
+{
+    Node *curr = head;
+
+    while (curr != NULL)
+    {
+        cout << curr->key << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
+
+void sort(Node *&head)
+{
+    Node *curr = head;
+    Node *next = NULL;
+    int temp;
+
+    if (head == NULL)
+    {
+        return;
+    }
+    else
+    {
+        while (curr != NULL)
+        {
+            next = curr->next;
+
+            while (next != NULL)
+            {
+                if (curr->key > next->key)
+                {
+                    temp = curr->key;
+                    curr->key = next->key;
+                    next->key = temp;
+                }
+                next = next->next;
+            }
+            curr = curr->next;
+        }
+    }
+}
+
+void reverse(Node *&head)
+{
+    Node *prev = NULL;
+    Node *curr = head;
+    Node *next = NULL;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
+
+void rotate(Node *&head, int k)
+{
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next = NULL;
+    int count = 0;
+
+    while (curr != NULL && count < k)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    head = prev;
+    while (curr != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = next;
+}
+
+void iShift(Node *&head, int k)
+{
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *next = NULL;
+    int count = 0;
+
+    while (curr != NULL && count < k)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+        count++;
+    }
+    head = prev;
+    while (curr != NULL)
+    {
+        curr = curr->next;
+    }
+    curr->next = next;
+}
+
+void clear(Node *&head)
+{
+    Node *curr = head;
+    Node *next = NULL;
+
+    while (curr != NULL)
+    {
+        next = curr->next;
+        delete curr;
+        curr = next;
+    }
+    head = NULL;
+}
+
