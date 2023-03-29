043a1472602c24f4fcad2c261f8e8da7f550ea1a
Irian Durian <durianirian@gmail.com>
2023-03-29 00:21:34 -0500

    Update LetterMatchID.cpp

--- Programming 3/Program set 2/LetterMatchID.cpp
+++ Programming 3/Program set 2/LetterMatchID.cpp
@@ -1,9 +1,54 @@
 #include <iostream>
+#include <stack>
 #include <string>
-#include <algorithm>
 
-using namspace std;
+bool is_properly_ordered(const std::string &input)
+{
+    // Create a stack for the characters.
+    std::stack<char> s;
 
-int main(){
-    
+    // Iterate through each character in the string.
+    for (const char &c : input)
+    {
+        // If the stack is empty or the character is not a matching pair
+        // with the top of the stack, push the character onto the stack.
+        if (s.empty() || abs(c - s.top()) != 25)
+        {
+            s.push(c);
+        }
+        // Otherwise, pop the top of the stack.
+        else
+        {
+            s.pop();
+        }
+    }
+
+    // If the stack is empty, the string is properly ordered.
+    return s.empty();
+}
+
+int main()
+{
+    std::string input;
+    char run_again;
+
+    do
+    {
+        std::cout << "Enter the string: ";
+        std::cin >> input;
+
+        if (is_properly_ordered(input))
+        {
+            std::cout << "YES" << std::endl;
+        }
+        else
+        {
+            std::cout << "NO" << std::endl;
+        }
+
+        std::cout << "Run again (Y/N): ";
+        std::cin >> run_again;
+    } while (run_again == 'Y' || run_again == 'y');
+
+    return 0;
 }
